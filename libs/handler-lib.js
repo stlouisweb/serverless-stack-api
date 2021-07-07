115a7a3ceeefe517e5443d6b99c67a5381b858e6
Jeremy Plack <stlouisweb@gmail.com>
2021-07-07 15:14:52 -0500

    Adding CORS to our Serverless API

--- libs/handler-lib.js
+++ libs/handler-lib.js
@@ -13,6 +13,10 @@ export default function handler(lambda) {
         return {
             statusCode,
             body: JSON.stringify(body),
+            headers: {
+                "Access-Control-Allow-Origin": "*",
+                "Access-Controll-Allow-Credentials": true,
+            },
         };
     };
 }
